a3c5edc9c15c6b5b1e343aac16e500a6c96134c5
Masud Kabir <ksmasud@gmail.com>
2022-10-17 10:56:54 +1100

    Add files via upload

---
+++ thegurdian.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\news_search\Services;
+
+use Drupal\news_search\params;
+
+
+class TheGuardian extends Params {
+    public $api_key = db3df076-d595-4cf4-bf29-c1b3e83ee5df;
+    public $param = array();
+    public function __construct($api_key) {
+       $this->api_key = $api_key;
+    }
+
+    //tags
+    public function get_tags() {
+        $url = "https://content.guardianapis.com/tags?".$this->determine();
+        $response = file_get_contents($url);
+        $json = json_decode($response);
+        return $json->response->results;
+    }
+
+    //sections
+    public function get_sections (){
+        $url = "https://content.guardianapis.com/sections?".$this->determine();
+        $response = file_get_contents($url);
+        $json = json_decode($response);
+        return $json->response->results;
+    }
+
+    //editions
+    public function get_editions (){
+        $url = "https://content.guardianapis.com/editions?".$this->determine();
+        $response = file_get_contents($url);
+        $json = json_decode($response);
+        return $json->response->results;
+    }
+
+    //content
+    public function get_articles() {
+        $url = "https://content.guardianapis.com/search?".$this->determine();
+        $response = file_get_contents($url);
+        $json = json_decode($response);
+        return $json->response->results;
+    }
+
+    //singetime
+    public function get_article($id) {
+        $url = "https://content.guardianapis.com/".$id."?".$this->determine();
+        $response = file_get_contents($url);
+        $json = json_decode($response);
+        return $json->response->content;
+    }
+
+    //detemining quaries
+    public function determine() {
+        if(count($this->param) == 0){
+             return http_build_query(array('api-key' => $this->api_key));
+        } else {
+            return http_build_query(array_merge($this->param, array('api-key' => $this->api_key)));
+        }
+    }
+}
+?>
